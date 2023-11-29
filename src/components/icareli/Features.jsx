6a8e7649f085bb529cd3ef90742ba9a46afb43f5
baguzbw <bagusbwicaksono5@gmail.com>
2023-11-29 19:13:23 +0700

    Fix Features ICARELI

--- src/components/icareli/Features.jsx
+++ src/components/icareli/Features.jsx
@@ -10,29 +10,27 @@ const Features = () => {
       <div className="flex flex-wrap md:flex-nowrap justify-center md:justify-between items-center mb-14">
         <div className="flex flex-col items-center">
           <LazyLoadImage effect="blur" src={Icareli2} loading="lazy" alt="Gambaran dari Detail Artikel" className="about-cloud-image waterfall w-full h-full mt-8" />
-          <h1 className="mt-2 font-plus-jakarta text-sm text-[#707072]">Gambaran dari Detail Artikel.</h1>
+          <h1 className="font-plus-jakarta text-sm text-[#707072]">Landing Page ICARELI.</h1>
         </div>
       </div>
       <div className="w-full md:w-1/2 mx-auto px-4 md:px-8 lg:px-16">
         <h1 className="text-lg text-primary font-bold font-plus-jakarta text-[#5E5E63] ">Features</h1>
         <h1 className="mt-3 text-2xl text-primary font-bold font-plus-jakarta ">Multiple Data</h1>
-        <p className="mb-4 mt-3 font-plus-jakarta text-lg">
-          Dalam pembuatan fitur artikel, kualitas konten yang disajikan menjadi topik utama dari fitur ini. Untuk mengoptimalkan pengalaman pengunjung, tampilan visual yang menarik dan responsif sangat diperhatikan. Desain interface yang
-          intuitif dapat mempermudah pengunjung menavigasi situs, membaca artikel dengan nyaman, dan menemukan informasi yang mereka cari yang berkaitan dengan budaya di Joglo Bale Agung Cendana.
+        <p className="mb-4 mt-3 font-plus-jakarta text-lg text-justify">
+          Dengan fitur Multiple Data, kita dapat mengubah, menambah, atau menghapus data di seluruh website.Dalam contoh multiple data, jika Anda ingin mengubah contoh ke konferensi tahun 2021, maka semua data yang berkaitan dengan
+          konferensi tersebut akan ditampilkan. Sebaliknya, jika Anda memilih konferensi tahun 2023, hal yang sama akan terjadi.
         </p>
       </div>
       <div className="flex flex-wrap md:flex-nowrap justify-center md:justify-between items-center mb-14">
         <div className="flex flex-col items-center">
           <LazyLoadImage effect="blur" src={Icareli3} loading="lazy" alt="Produk & Motif Core Kampung Batik." className="about-cloud-image waterfall w-full h-full mt-8" />
-          <h1 className="mt-2 font-plus-jakarta text-sm text-[#707072]">Produk & Motif Core Kampung Batik.</h1>
+          <h1 className="font-plus-jakarta text-sm text-[#707072]">Informasi Guide Program.</h1>
         </div>
       </div>
       <div className="w-full md:w-1/2 mx-auto px-4 md:px-8 lg:px-16">
         <h1 className="text-2xl text-primary font-bold font-plus-jakarta ">Informasi ICARELI</h1>
-        <p className="mb-10 mt-3 font-plus-jakarta text-lg">
-          Fitur ini dirancang untuk memberikan pengalaman yang lengkap dan informatif kepada pengunjung terkait produk-produk dan motif khas dari Joglo Bale Agung Cendana. Tampilan menggunakan grouping produk & motif yang jika diclick akan
-          memberikan informasi lebih lanjut tentang produk atau motif yang diclick. Melalui fitur ini pengunjung diberikan kesempatan untuk mengeksplorasi dan mengenal lebih dalam tentang produk & motif batik yang ditawarkan di Joglo Bale
-          Agung Cendana.
+        <p className="mb-10 mt-3 font-plus-jakarta text-lg text-justify">
+          Fitur Informasi mencakup banyak hal, seperti informasi tentang pembicara, artikel tentang konferensi yang terjadi, jadwal konferensi, komite yang berpartisipasi, pedoman program, dan informasi tentang registrasi submission.
         </p>
       </div>
     </div>
