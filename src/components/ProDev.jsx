526aa68ca23584a663a64f36543a49fc25d314d7
baguzbw <bagusbwicaksono5@gmail.com>
2023-11-29 20:19:57 +0700

    Change third card

--- src/components/ProDev.jsx
+++ src/components/ProDev.jsx
@@ -2,11 +2,11 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import logo1 from "./assets/Joglo.svg";
-import logo2 from "./assets/among.svg";
 import illust3 from "./assets/cardicareli.svg";
 import icareli from "./assets/icareli.svg";
 import illust1 from "./assets/kampung.png";
-import illust2 from "./assets/porto.png";
+import logo2 from "./assets/logo_cleon.png";
+import illust2 from "./assets/olt.png";
 
 const ProjectCard = () => (
   <div className="flex flex-col items-center px-4 sm:px-6 md:px-8 lg:px-12 xl:px-0">
@@ -68,13 +68,13 @@ const ProjectCard = () => (
     <div className="grid grid-cols-1 lg:grid-cols-2 bg-[#ECEFF1] shadow-md rounded-3xl w-full lg:w-[850px] h-auto lg:h-[380px] mx-auto mt-10 overflow-hidden group transition-transform duration-500 ease-in-out transform hover:scale-105">
       <div className="p-4 lg:p-12 flex flex-col justify-center">
         <div className="flex flex-col items-start mb-4">
-          <img src={logo2} alt="Logo" className="w-10 h-10" loading="lazy" />
+          <img src={logo2} alt="Logo" className="w-16 h-8 mb-2" loading="lazy" />
           <h2 className="mt-2 text-4xl font-bold font-plus-jakarta text-center">
-            <span className="block w-full py-2 font-plus-jakarta text-transparent bg-clip-text leading-12 bg-gradient-to-r from-[#323235] to-[#5E5E63] lg:inline">Web Portofolio</span>
+            <span className="block w-full py-2 font-plus-jakarta text-transparent bg-clip-text leading-12 bg-gradient-to-r from-[#323235] to-[#5E5E63] lg:inline">OLT Monitoring</span>
           </h2>
           <p className="text-sm text-[#5E5E63] font-semibold font-plus-jakarta mt-2">FRONT END DEV&nbsp; • &nbsp;2023</p>
         </div>
-        <p className="text-lg text-[#5E5E63] font-plus-jakarta mb-4">Website portofolio yang mencakup project dan pengalaman.</p>
+        <p className="text-lg text-[#5E5E63] font-plus-jakarta mb-4">Website monitoring untuk Optical Line Termination.</p>
         <button className="bg-[#c6c6c6]  font-plus-jakarta rounded-md px-4 py-2 border-2 border-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200" style={{ width: "260px", height: "48px", borderRadius: "12px" }}>
           Studi kasus ini belum siap
         </button>
